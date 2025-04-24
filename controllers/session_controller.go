4e5ce617aa6c38a45fe41d6811b9d56522367eb8
jorge-ortiz-sp <jorge.ortiz@sailpoint.com>
2025-04-23 18:24:13 -0600

    add JWT to return auth tokens

--- controllers/session_controller.go
+++ controllers/session_controller.go
@@ -2,10 +2,14 @@ package controllers
 
 import (
 	"net/http"
+	"os"
+	"time"
 	"yorch-devs/bookstore-golang-postgres/controllers/concerns"
 	"yorch-devs/bookstore-golang-postgres/controllers/controller_structs"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/google/uuid"
 )
 
 func Login(c *gin.Context) {
@@ -30,5 +34,30 @@ func Login(c *gin.Context) {
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"message": "User has successfully authenticated"})
+	token, err := generateAuthToken(validUsername.User.Id)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Something went wrong when generating auth token"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"auth_token": token})
+}
+
+func generateAuthToken(userId uuid.UUID) (string, error) {
+	var jwtSecret = []byte(os.Getenv("SECRET_KEY"))
+
+	claims := jwt.MapClaims{
+		"user_id": userId,
+		"exp":     time.Now().Add(time.Second * 30).Unix(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	signedToken, err := token.SignedString(jwtSecret)
+
+	if err != nil {
+		return "", err
+	}
+
+	return signedToken, nil
 }
